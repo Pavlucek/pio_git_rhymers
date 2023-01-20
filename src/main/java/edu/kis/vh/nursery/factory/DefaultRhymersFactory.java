9b612f20aaea5ede89099185c831f4d81a3373ba
Pavlucek <p.morozewicz99@gmail.com>
2023-01-20 22:36:43 +0100

    1.13.2 Once more repaired formatting of files ctrl+alt+L

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,27 +4,28 @@ import edu.kis.vh.nursery.defaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
+
 //Zweryfikowałem działanie kombinacji alt + stzałka pozwala mi na przeskakiwanie pomiędzy otwartymi plikami.
 public class DefaultRhymersFactory implements Rhymersfactory {
 
-	@Override
-	public defaultCountingOutRhymer GetStandardRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetStandardRhymer() {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFalseRhymer() {
-		return new defaultCountingOutRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFalseRhymer() {
+        return new defaultCountingOutRhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetFIFORhymer() {
-		return new FIFORhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetFIFORhymer() {
+        return new FIFORhymer();
+    }
 
-	@Override
-	public defaultCountingOutRhymer GetHanoiRhymer() {
-		return new HanoiRhymer();
-	}
+    @Override
+    public defaultCountingOutRhymer GetHanoiRhymer() {
+        return new HanoiRhymer();
+    }
 
 }
